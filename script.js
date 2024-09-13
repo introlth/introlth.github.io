a56a6624be84670a5b486613feb88a42381ada98
leetaehun <taehun1231@naver.com>
2024-09-13 12:11:37 +0900

    수정 전

--- script.js
+++ script.js
@@ -3,18 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const userRecommendationsLink = document.getElementById('user-recommendations');
   const contentSection = document.getElementById('content');
 
-  movieListLink.addEventListener('click', (event) => {
-      event.preventDefault();
-      contentSection.innerHTML = `
-          <h2>천만 영화</h2>
-          <ul>
-              <li>극한직업</li>
-              <li>신과함께</li>
-              <li>명량</li>
-              <li>기생충</li>
-          </ul>
-      `;
-  });
 
   userRecommendationsLink.addEventListener('click', (event) => {
       event.preventDefault();
